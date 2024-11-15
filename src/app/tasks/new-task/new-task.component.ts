2f60bf6d814fe938535cf29da11f8e5455c140cf
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-11-15 17:04:32 +0100

    feat: add two-way binding to inputs

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,14 +1,21 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
   @Output() cancel = new EventEmitter<boolean>();
+  enteredTitle = '';
+  enteredSummary = '';
+  enteredDate = '';
+  // enteredTitle = signal('');
+  // enteredSummary = signal('');
+  // enteredDate = signal('');
 
   onCancel() {
     this.cancel.emit(false);
