8eaf85a307531f48e152d9ef79ba4dca02a85885
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-11-15 16:11:45 +0100

    feat: add active class dynamically

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -37,6 +37,7 @@ export class UserComponent {
     // @Input({ required: true }) avatar!: string;
     // @Input({ required: true }) name!: string;
     @Input({required: true}) user!: User;
+    @Input({required: true}) selected!: boolean;
     @Output() select = new EventEmitter<string>();
     // avatar = input.required<string>();
     // name = input.required<string>();
